42aafba5af1ecb29675082545087b163bfbba4b3
vgotsuliak <liddder@gmail.com>
2016-04-18 12:05:01 +0300

    remove XML department mapper, use annotations instead

--- src/main/java/ua/com/gotsuliak/firstproject/mapper/DepartmentMapper.java
+++ src/main/java/ua/com/gotsuliak/firstproject/mapper/DepartmentMapper.java
@@ -1,9 +1,18 @@
 package ua.com.gotsuliak.firstproject.mapper;
 
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
 import ua.com.gotsuliak.firstproject.entity.Department;
+
 
 public interface DepartmentMapper {
 
+    @Results({
+            @Result(id = true, property = "id", column = "id"),
+            @Result(property = "name", column = "name")
+    })
+    @Select("Select name from department where id = #{id}")
     Department getDepartment(int id);
 
 }
