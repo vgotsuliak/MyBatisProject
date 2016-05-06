5ec8ae3fd71eb23334a5fc609b4805c975d24abd
vgotsuliak <liddder@gmail.com>
2016-05-06 16:21:12 +0300

    add fetching department when selecting employee

--- src/main/java/ua/com/gotsuliak/firstproject/mapper/EmployeeMapper.java
+++ src/main/java/ua/com/gotsuliak/firstproject/mapper/EmployeeMapper.java
@@ -1,11 +1,23 @@
 package ua.com.gotsuliak.firstproject.mapper;
 
+import org.apache.ibatis.annotations.One;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import ua.com.gotsuliak.firstproject.entity.Department;
 import ua.com.gotsuliak.firstproject.entity.Employee;
 
 public interface EmployeeMapper {
 
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "department", column = "id", one = @One(select = "getDepartment"))
+    })
     @Select("select * from employee where id=#{id}")
     Employee getEmployee(int id);
+
+    @Select("select * from department where id=#{id}")
+    Department getDepartment(int id);
 
 }
