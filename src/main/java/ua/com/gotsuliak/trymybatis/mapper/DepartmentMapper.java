ec8f3dbe184b53001c9d35657f0404fdf3686b52
vgotsuliak <liddder@gmail.com>
2016-05-10 15:57:32 +0300

    add xml departmentMapper, because xml is better way of working with mybatis

--- src/main/java/ua/com/gotsuliak/trymybatis/mapper/DepartmentMapper.java
+++ src/main/java/ua/com/gotsuliak/trymybatis/mapper/DepartmentMapper.java
@@ -1,9 +1,5 @@
 package ua.com.gotsuliak.trymybatis.mapper;
 
-import org.apache.ibatis.annotations.Many;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
 import ua.com.gotsuliak.trymybatis.entity.Department;
 import ua.com.gotsuliak.trymybatis.entity.Employee;
 
@@ -12,25 +8,10 @@ import java.util.List;
 
 public interface DepartmentMapper {
 
-    /**
-     * Annotation @Results is redundant here because property names are the same as column names.
-     * Leave it for clearness
-     */
-    @Results({
-            @Result(id = true, property = "id", column = "id"),
-            @Result(property = "name", column = "name")
-    })
-    @Select("Select * from department where id = #{id}")
     Department getDepartment(int id);
 
-    @Select("Select * from department")
-    @Results({
-            @Result(property = "id", column = "id"),
-            @Result(property = "employees", column = "id", many = @Many(select = "getEmployees"))
-    })
     List<Department> getDepartments();
 
-    @Select("select * from employee where department_id = #{id}")
     List<Employee> getEmployees(int id);
 
 }
